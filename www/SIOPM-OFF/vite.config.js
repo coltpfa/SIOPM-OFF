fe02a553f58b4f542a5181c78ab965ad590c2ec5
coltpfa <coltpfa@gmail.com>
2023-07-20 21:56:51 -0300

    add plugin inject jquery

--- www/SIOPM-OFF/vite.config.js
+++ www/SIOPM-OFF/vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import inject from "@rollup/plugin-inject";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/menu.js'],
             refresh: true,
+        }),
+        inject({
+            $: 'jquery', // this caused warnings for all my scss files that had $variable
+            jQuery: 'jquery',
         }),
     ],
 });
